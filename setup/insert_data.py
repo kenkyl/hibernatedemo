d768cda08eb16c03a68a20ce99eebdfd31ba722b
kenkyl <kylekennedy@Kyles-MBP-2.attlocal.net>
2021-01-26 23:17:53 -0600

    docker-compose working. added wait-for-it dependency, dockerized insert_data script

--- setup/insert_data.py
+++ setup/insert_data.py
@@ -2,17 +2,19 @@ import mysql.connector
 # import redis
 import string
 import random
+import sys
+import os
 
 REDIS_HOST='localhost'
 REDIS_PORT=6379
 REDIS_PASSWORD='password'
 
-MYSQL_CONNECTION='localhost'
-MYSQL_PORT='3306'
-MYSQL_DATABASE='testdb'
-MYSQL_USER='root'
-MYSQL_PASSWORD='password'
-MYSQL_TABLE='init_object'
+MYSQL_HOST= os.environ.get('MYSQL_HOST') if (os.environ.get('MYSQL_HOST') is not None) else 'localhost' 
+MYSQL_PORT= os.environ.get('MYSQL_PORT') if (os.environ.get('MYSQL_PORT') is not None) else '3306'
+MYSQL_DB= os.environ.get('MYSQL_DB') if (os.environ.get('MYSQL_DB') is not None) else 'testdb'
+MYSQL_USER= os.environ.get('MYSQL_USER') if (os.environ.get('MYSQL_USER') is not None) else 'root'
+MYSQL_PASS= os.environ.get('MYSQL_PASS') if (os.environ.get('MYSQL_PASS') is not None) else 'password'
+MYSQL_TABLE= 'init_object'
 
 NUM_ITEMS = 2000
 ITEM_LENGTH = 250
@@ -23,13 +25,23 @@ def main():
 
     # connect to MySql
     print('loading data into mysql')
+    # attempts = 0ß
+
+    # attempt to connect to the database
+    # while (True):
+    #     try:
     connection = mysql.connector.connect(
-        host=MYSQL_CONNECTION,
+        host=MYSQL_HOST,
         port=MYSQL_PORT,
-        database=MYSQL_DATABASE,
+        database=MYSQL_DB,
         user=MYSQL_USER,
-        password=MYSQL_PASSWORD
-    )
+        password=MYSQL_PASS
+     )
+        # except:
+        #     print('could not connect, retrying...')
+        #     attempts += 1
+        #     if (attempts > 500000):
+        #         sys.exit('failed to connect to mysql! self-destruct!')
     print(connection)
 
     # create table (if it doesn't exist)
@@ -43,9 +55,14 @@ def main():
         value5 VARCHAR(255)
     )
     """
-    with connection.cursor() as cursor:
-        cursor.execute(create_table_query)
-        connection.commit
+
+    cursor = connection.cursor()
+    cursor.execute(create_table_query)
+    connection.commit
+
+    # with connection.cursor() as cursor:
+    #     cursor.execute(create_table_query)
+    #     connection.commit
 
     # insert data 
     items_list = []
@@ -63,11 +80,18 @@ def main():
 
         items_list.append((str(i), item_value1, item_value2, item_value3, item_value4, item_value5))
         # r.hset(f'items:{i}', mapping={'value1': item_value1, 'value2': item_value2, 'value3': item_value3, 'value4': item_value4, 'value5': item_value5})
-    with connection.cursor() as cursor:
-        cursor.executemany(insert_query, items_list)
-        connection.commit()
+
+    curson = connection.cursor()
+    cursor.executemany(insert_query, items_list)
+    connection.commit()
+
+    # with connection.cursor() as cursor:
+    #     cursor.executemany(insert_query, items_list)
+    #     connection.commit()
 
     print('data load complete')
+    quit()
+    
 
 
 if __name__ == "__main__":
